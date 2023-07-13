1da15b479126f5200dffd450adfe836496d199df
ilias <iliaschokairi@gmail.com>
2023-07-13 13:27:47 -0700

    creat a C program that size multiple data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	/*main function that size data type*/
+	printf("Size of a char:%lu byte(s)\n", sizeof(char));
+	printf("Size of a char:%lu byte(s)\n", sizeof(int));
+	printf("Size of a char:%lu byte(s)\n", sizeof(long int));
+	printf("Size of a char:%lu byte(s)\n", sizeof(long long));
+	return (0);
+}
