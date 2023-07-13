7d61f5f0c5aed3fa652a72808be15c22fbae3c38
ilias <iliaschokairi@gmail.com>
2023-07-13 14:04:29 -0700

    creat a C program that puts a file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'creat a C program that use puts function'
+ * Return: Always 0 (Success)
+ */
 int main(void)  /* using puts function to write a string */
 {
 	puts("\"Programming is like building a multilingual puzzle");
