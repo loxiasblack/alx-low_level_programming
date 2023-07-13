f501c46ec9f3c83a412bfb81f098d08cb1732b4b
ilias <iliaschokairi@gmail.com>
2023-07-13 16:22:32 -0700

    creat a C program that print without puts or printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: 'print without  printf or puts  functions'
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int length = sizeof(str) - 1;
+
+	write(STDOUT_FILENO, str, length);
+	return (1);
+}
