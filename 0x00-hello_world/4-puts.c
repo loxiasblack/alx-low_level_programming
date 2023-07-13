e3159d0678b8613920c0f9dc108694aaf0063b80
ilias <iliaschokairi@gmail.com>
2023-07-13 12:31:35 -0700

    creat file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-/*
- *printing the strint with puts function
- */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-
+    puts("\"Programming is like building a multilingual puzzle"); /* using puts function to write a string */
     return 0;
 }
+
