93fac6809283e24172b5ec9b6985cf28db464be6
ilias <iliaschokairi@gmail.com>
2023-07-13 13:37:04 -0700

    creat a C program that size multiple data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 int main(void)
 {
 	/*main function that size data type*/
-	printf("Size of a char:%lu byte(s)\n", sizeof(char));
-	printf("Size of an int:%lu byte(s)\n", sizeof(int));
-	printf("Size of a long int:%lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int:%lu byte(s)\n", sizeof(long long));
-	printf("Size of a float:%lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
