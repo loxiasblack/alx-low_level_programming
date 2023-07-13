202e7d197e66254b8d3a882781a102d557933e60
ilias <iliaschokairi@gmail.com>
2023-07-13 12:40:47 -0700

    puts fonction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
-
+/* using puts function to write a string */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle"); /* using puts function to write a string */
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return() 0;
 }
-
