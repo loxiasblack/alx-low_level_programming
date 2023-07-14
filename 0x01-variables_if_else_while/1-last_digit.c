610b9c02e9aecfa3c1f8840dcbf467a4b851ad99
ilias <iliaschokairi@gmail.com>
2023-07-14 09:49:23 -0700

    creat a c program that compare the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,18 +11,18 @@
 int main(void)
 {
 	int n;
-	int last_digit;
+	int digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	scanf("%d", &n);
-	last_digit = n % 10;
-	if (n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	else if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	digit = n % 10;
+	if (digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	return (0);
 }
