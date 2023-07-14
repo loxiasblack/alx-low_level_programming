72c866af9e30804e73a31683ce7951453b6c6518
ilias <iliaschokairi@gmail.com>
2023-07-14 04:00:52 -0700

    creat a file that tell i the n variable are positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,10 @@ int main(void)
 	/* your code goes there */
 	scanf("%d", &n);
 	if (n > 0)
-	{
 	    printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 	    printf("%d is zero\n", n);
-	}
 	else
-	{
 	    printf("%d is negative\n", n);
-	}
 	return (0);
 }
