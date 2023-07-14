8d76ffd3d5d0fd36d7f7715684c45465dc378a85
ilias <iliaschokairi@gmail.com>
2023-07-14 04:43:24 -0700

    creat file that show if n is negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: 'creat a program that tell if n is negative or positive'
+ * Return: Always 0 (sucess)
+*/
 int main(void)
 {
 	int n;
@@ -13,10 +18,10 @@ int main(void)
 	/* your code goes there */
 	scanf("%d", &n);
 	if (n > 0)
-	    printf("%d is positive\n", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	    printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	else
-	    printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
