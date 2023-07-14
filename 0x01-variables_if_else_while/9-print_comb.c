3a4146cfdd054f22f806f1f1e089aa7283e33894
ilias <iliaschokairi@gmail.com>
2023-07-14 15:28:06 -0700

    creat a file that print 0123456789 separated with commas space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print all digit number with commas'
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+int i;
+
+while (i <= 9)
+{
+    putchar('0' + i);
+    if (i < 9)
+	    putchar(',');
+    putchar(' ');
+    i++;
+}
+putchar('\n');
+return (0);
+}
