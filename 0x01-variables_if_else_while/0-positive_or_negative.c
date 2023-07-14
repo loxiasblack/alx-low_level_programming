6feb8a0fd4b5ee2780a2e31cd2b01d49b1f16207
ilias <iliaschokairi@gmail.com>
2023-07-14 03:45:36 -0700

    creat a c program that use if esle

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	 printf("is positive");
+	 printf("is positive\n");
 	}
 	else if (n == 0)
 	{
-	 printf("is zero");
+	 printf("is zero\n");
 	}
 	else
 	{
-	printf("is negative");
+	printf("is negative\n");
 	}
 	return (0);
 }
