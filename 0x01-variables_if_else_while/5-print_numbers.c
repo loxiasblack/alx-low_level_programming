631fbbfb400e2d3735b809bf06ce90b1f70b59b2
ilias <iliaschokairi@gmail.com>
2023-07-14 13:05:57 -0700

    creat a c program that print all number digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print all single digit base on 10'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+
+for (i = 0 ; i < 10 ; i++)
+{
+	printf("%d", i);
+}
+printf("\n");
+return (0);
+}
