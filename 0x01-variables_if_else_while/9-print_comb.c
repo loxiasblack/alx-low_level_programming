9dfd25b0af52d0eb8de270839150577fef4ba496
ilias <iliaschokairi@gmail.com>
2023-07-14 15:36:32 -0700

    creat a file that print 0123456789

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,17 +4,18 @@
  * Description: 'print all digit number with commas'
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 int i;
 
 while (i <= 9)
 {
-    putchar('0' + i);
-    if (i < 9)
-	    putchar(',');
-    putchar(' ');
-    i++;
+	putchar('0' + i);
+	if (i < 9)
+		putchar(',');
+	if (i < 9)
+		putchar(' ');
+	i++;
 }
 putchar('\n');
 return (0);
