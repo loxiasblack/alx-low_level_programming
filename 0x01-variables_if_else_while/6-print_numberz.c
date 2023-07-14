29fd66d6a0bc95987a1d036eb41242be22605730
ilias <iliaschokairi@gmail.com>
2023-07-14 13:11:26 -0700

    creat a file that print 0123456789 only with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print all single digit base on 10 but only withchar'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "0123456789";
+int i = 0;
+
+for (i = 0 ; i < 11 ; i++)
+{
+	putchar(str[i]);
+}
+putchar('\n');
+return (0);
+}
