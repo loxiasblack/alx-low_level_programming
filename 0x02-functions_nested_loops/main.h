a14dce6ee0a25fa29d71879255c9d40a3cb399a2
ilias <iliaschokairi@gmail.com>
2023-07-18 18:06:00 +0100

    creat a file that determine if the charcter is in lower case

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 int _putchar(char c);
 int print_alphabet();
 int print_alphabet_x10(void);
+int _islower(int c);
