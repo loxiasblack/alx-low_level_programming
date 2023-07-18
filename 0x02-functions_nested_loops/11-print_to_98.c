c5ea115caf2bce606b51e6d0a772707b2f765d0a
ilias <iliaschokairi@gmail.com>
2023-07-19 00:54:52 +0100

    creat 11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98- function that prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: start of the count
+ * Return: 0 sucess
+ */
+void print_to_98(int n)
+{
+if (n < 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+}
+else
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+}
+printf("98\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,3 +18,4 @@ void jack_bauer(void);
 int add(int, int);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
