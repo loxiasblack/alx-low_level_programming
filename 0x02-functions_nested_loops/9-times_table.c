471d04309b64b17f479bcbbf46c9c39d47b6c7ea
ilias <iliaschokairi@gmail.com>
2023-07-18 23:41:06 +0100

    creat a table number

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,24 +15,27 @@ void times_table(void)
 		{
 			int k = i * j;
 
-			if (k < 10)
+			if (j == 0)
 			{
-				_putchar(' ');
 				_putchar('0' + k);
 			}
-			else
-			{
-				_putchar('0' + (k / 10));
-				_putchar('0' + (k % 10));
-			}
-			if (j < 9)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			j++;
 		}
-		putchar('\n');
+		_putchar('\n');
 		i++;
 	}
 }
