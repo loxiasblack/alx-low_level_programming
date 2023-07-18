349a5ea18791d29d8aa57ef13f0e16e7c94be228
ilias <iliaschokairi@gmail.com>
2023-07-18 14:29:20 +0100

    creat a program that print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - Entry point
+ * Description: 'print _putchar'
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-    char string[] = "_putchar";
-    int i;
+	char string[] = "_putchar";
+	int i;
 
-    i = 0;
-    while (i <8 )
-    {
-        putchar(string[i]);
-        i++;
-    }
-    putchar('\n');
-    return (0);
+	i = 0;
+	while (i < 8)
+	{
+		putchar(string[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
