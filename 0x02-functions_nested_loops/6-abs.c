a19e7161538c694567059464a6ab4a174c59c2dc
ilias <iliaschokairi@gmail.com>
2023-07-18 20:04:24 +0100

    creat a file that return the absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description: 'return the absolute of n'
+ * @num: character
+ * Return: Always num.
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
