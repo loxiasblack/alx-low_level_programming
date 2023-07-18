0041a21cf19c6643d11760382440e1b6c175d391
ilias <iliaschokairi@gmail.com>
2023-07-18 23:55:49 +0100

    creat add function

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,15 @@
 #include "main.h"
-
+/**
+ * add - Entry point
+ * Description: 'add to numbers'
+ * @a: first integer
+ * @b: second integer
+ * Return: Always results.
+ */
 int add(int a, int b)
 {
-    int results;
-    results = a + b;
-    return (results);
+	int results;
+
+	results = a + b;
+	return (results);
 }
