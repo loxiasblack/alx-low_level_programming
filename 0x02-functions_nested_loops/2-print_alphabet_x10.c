f616b917a707d853c196a07be061c71e20bef08e
ilias <iliaschokairi@gmail.com>
2023-07-18 17:15:28 +0100

    creat a file that print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * prin_alphabet_x10 - Entry point
+ * Description: 'print the alphabet 10 times'
+ * Return: Always 0 (Success)
+ */
+
+int print_alphabet_x10(void)
+{
+	char alph[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 26)
+		{
+			_putchar(alph[j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,4 @@
 
 int _putchar(char c);
 int print_alphabet();
+int print_alphabet_x10(void);
