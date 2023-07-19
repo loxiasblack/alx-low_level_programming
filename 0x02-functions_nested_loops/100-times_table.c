e85e3b7bb8fe94ca2214baa9c5d914b0469dca78
ilias <iliaschokairi@gmail.com>
2023-07-19 13:26:40 +0100

    creat a table for n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,39 +6,40 @@
  */
 void print_times_table(int n)
 {
-	int i=0;
-    int j;
-    
-    while (i <= n)
-    {
-        j = 0;
-        
-        while (j <= n){
-            int k = i * j;
-            
-            if (j == 0)
-            {
-                printf("%d", k);
-            }
-            if (k <= 9 && j != 0)
-            {
-                printf(",   %d", k);
-            }
-            if (k >= 10 && k <= 99)
-            {
-                printf(",  %d", k);
-            }
-            if (k > 99 && k <= 999)
-            {
-                printf(", %d", k);
-            }
-            else if (k >= 1000)
-            {
-                printf(",%d", k);
-            }
-            j++;
-        }
-        putchar('\n');
-        i++;
-    }
+	int i = 0;
+	int j;
+
+	while (i <= n)
+	{
+		j = 0;
+
+		while (j <= n)
+		{
+			int k = i * j;
+
+			if (j == 0)
+			{
+				printf("%d", k);
+			}
+			if (k <= 9 && j != 0)
+			{
+				printf(",   %d", k);
+			}
+			if (k >= 10 && k <= 99)
+			{
+				printf(",  %d", k);
+			}
+			if (k > 99 && k <= 999)
+			{
+				printf(", %d", k);
+			}
+			else if (k >= 1000)
+			{
+				printf(",%d", k);
+			}
+			j++;
+		}
+		putchar('\n');
+		i++;
+	}
 }
