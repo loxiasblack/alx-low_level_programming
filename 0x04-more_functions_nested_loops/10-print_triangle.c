60ec70c4bbcdcdb37a3e0348f425fd767fe9e91e
ilias <iliaschokairi@gmail.com>
2023-07-20 21:25:53 +0100

    creat a file that print a triangle with squares

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_triangle - print the size of triangle
+ * @size: the size
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i = 0, j;
+
+	if (size > 0)
+	{
+		while (i <= size)
+		{
+			j = size;
+			while (j--)
+			{
+				if (j >= i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
