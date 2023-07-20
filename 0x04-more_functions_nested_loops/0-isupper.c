020270a047cab63d8eccf774fb955c960fd71e46
ilias <iliaschokairi@gmail.com>
2023-07-20 11:12:01 +0100

    creat a file that return 1 or 0 if the char is upper or no

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+# include "main.h"
+/**
+ * _isupper - returns the largest of 3 numbers
+ * @c: char
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
