794fd88f228677ed7c60adacad9747ee78c83fd7
ilias <iliaschokairi@gmail.com>
2023-07-20 18:53:35 +0100

    creat a file that print 1 to 100 and replace by the name wanted if much the condition

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,22 @@
  */
 int main(void)
 {
-int i = 1;
-while (i < 101)
-{
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
-printf("Fizz");
-else if (i % 5 == 0)
-printf("Buzz");
-else
-printf("%d", i);
-if (i < 100)
-printf(" ");
-i++;
-}
-printf("\n");
-return (0);
+	int i = 1;
+
+	while (i < 101)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
