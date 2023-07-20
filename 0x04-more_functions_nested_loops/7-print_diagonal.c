e76d7a97f38d3b973c2d0050ce3c2d561c156531
ilias <iliaschokairi@gmail.com>
2023-07-20 15:07:39 +0100

    creat a file that creat n diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - print form 0 to 14 in 10 line
+ * @n: the number of diagonal desired
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+}
