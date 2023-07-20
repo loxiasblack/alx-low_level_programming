0a76f571c533cd6b9bc4ad9d8a26b0b49da5158c
ilias <iliaschokairi@gmail.com>
2023-07-20 15:32:44 +0100

    creat a file that n squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - print the size of square
+ * @size: the sizw
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int i = 0, j;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
