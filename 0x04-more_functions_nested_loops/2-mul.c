737a94c901bfc8efc617cc70074263124dfd972d
ilias <iliaschokairi@gmail.com>
2023-07-20 11:52:04 +0100

    creat a file that return the mul of two int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * mul - determine if it's digit
+ * @a: int
+ * @b: int
+ * Return: c
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
