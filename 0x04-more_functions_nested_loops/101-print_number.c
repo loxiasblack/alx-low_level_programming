875e4cfd7852ec0b865684880f817d8e727c5e80
ilias <iliaschokairi@gmail.com>
2023-07-21 12:29:50 +0100

    creat a file that putchar numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,32 +1,37 @@
 #include "main.h"
-
+/**
+ * print_number - print the size of triangle
+ * @n: the value printed
+ * Return: nothing
+ */
 void print_number(int n)
 {
-    int divisor;
-    int temp;
+	int divisor;
+	int temp;
 
-    if (n == 0)
-    {
-	    _putchar('0');
-	    return;
-    }
-    if (n < 0)
-    {
-	    _putchar('-');
-	    n = -n;
-    }
-    divisor = 1;
-    temp = n;
-    while (temp >= 10)
-    {
-	    temp /= 10;
-	    divisor *= 10;
-    }
-    while (divisor >= 1)
-    {
-	    int digit = n / divisor;
-	    _putchar('0' + digit);
-	    n %= divisor;
-	    divisor /= 10;
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	divisor = 1;
+	temp = n;
+	while (temp >= 10)
+	{
+		temp /= 10;
+		divisor *= 10;
+	}
+	while (divisor >= 1)
+	{
+		int digit = n / divisor;
+
+		_putchar('0' + digit);
+		n %= divisor;
+		divisor /= 10;
+	}
 }
