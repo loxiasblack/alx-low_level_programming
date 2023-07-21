98647179c059edc9df3edaea47b9274b75db792b
ilias <iliaschokairi@gmail.com>
2023-07-21 12:17:21 +0100

    creat a file that putchar numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void print_number(int n)
+{
+    int divisor;
+    int temp;
+
+    if (n == 0)
+    {
+	    _putchar('0');
+	    return;
+    }
+    if (n < 0)
+    {
+	    _putchar('-');
+	    n = -n;
+    }
+    divisor = 1;
+    temp = n;
+    while (temp >= 10)
+    {
+	    temp /= 10;
+	    divisor *= 10;
+    }
+    while (divisor >= 1)
+    {
+	    int digit = n / divisor;
+	    _putchar('0' + digit);
+	    n %= divisor;
+	    divisor /= 10;
+    }
+}
