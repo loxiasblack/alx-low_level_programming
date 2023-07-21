a0e596e5e3aae7c8b09ead13c8645edcdcc1b51d
ilias <iliaschokairi@gmail.com>
2023-07-21 20:57:13 +0100

    creat a program that print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,21 @@
 #include "main.h"
 /**
  * print_number - print the size of triangle
- * @n: the value printed
+ * @n: the number to be printed
  * Return: nothing
  */
 void print_number(int n)
 {
-	int divisor;
-	int temp;
+	unsigned int i = n;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -i;
 	}
-	divisor = 1;
-	temp = n;
-	while (temp >= 10)
+	if ((i / 10) > 0)
 	{
-		temp /= 10;
-		divisor *= 10;
+		print_number(i / 10);
 	}
-	while (divisor >= 1)
-	{
-		int digit = n / divisor;
-
-		_putchar('0' + digit);
-		n %= divisor;
-		divisor /= 10;
-	}
+	_putchar((i % 10) + '0');
 }
