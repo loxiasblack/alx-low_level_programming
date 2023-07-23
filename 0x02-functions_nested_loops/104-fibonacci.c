9c545c70dd175a1596b0e48a72c6ea697b1a152e
ilias <iliaschokairi@gmail.com>
2023-07-23 21:42:32 +0100

    creat a fibonnaci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: 'fibonnaci 98'
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-    unsigned long prev = 0;
-    unsigned long current = 1;
-    unsigned long next;
-    int n = 98;
+	unsigned long prev = 0;
+	unsigned long current = 1;
+	unsigned long next;
+	int n = 99;
 
-    while (n--)
-    {
-        next = current + prev;
-        printf("%lu, ", next);
-        prev = current;
-        current = next;
-        if (n == 0)
-            printf("\n");
-    }
-    return 0;
-    
+	while (n--)
+	{
+		next = current + prev;
+		if (n != 0)
+			printf("%lu, ", next);
+		prev = current;
+		current = next;
+		if (n == 0)
+			printf("%lu\n", next);
+	}
+	return (0);
 }
