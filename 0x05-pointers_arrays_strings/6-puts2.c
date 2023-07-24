49e8a3d12e5b613a8c66a7c6f4096454c4521d1c
ilias <iliaschokairi@gmail.com>
2023-07-24 23:10:43 +0100

    creat a program to puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - print a string,
+ * @str: the string
+ * Return: 0 sucess
+ */
+void puts2(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n += 2;
+	}
+	_putchar('\n');
+}
