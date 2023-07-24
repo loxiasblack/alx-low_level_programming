358c4962a8f9d0c1384a5c38cccc358b878296eb
ilias <iliaschokairi@gmail.com>
2023-07-24 19:57:33 +0100

    creat a program that print a string of character

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
