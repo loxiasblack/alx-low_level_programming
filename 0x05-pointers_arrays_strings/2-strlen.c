d2062271d1cdb9332bddc95a9a7e4f8c84f4669f
ilias <iliaschokairi@gmail.com>
2023-07-24 19:43:16 +0100

    creat a program that print the lenght of strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,16 @@
 #include "main.h"
-
+/**
+ * _strlen - swap value of int,
+ * @s: the string
+ * Return: len sucess
+ */
 int _strlen(char *s)
 {
-    int len = 0;
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-    return len;
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
