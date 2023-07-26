5d12ef0b363cdbb083a7b1e990ec8a5a7c0b0147
ilias <iliaschokairi@gmail.com>
2023-07-26 19:56:37 +0100

    creat a file that copy from string to other

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i] != '\0')
 	{
-		if (src[i] != '\0')
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
 		i++;
 	}
-	return dest;
-	
-	
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
