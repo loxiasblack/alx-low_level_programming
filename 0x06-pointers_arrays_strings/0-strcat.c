e309d2b6e55a39313fce6d3f9e729613ad602eb1
ilias <iliaschokairi@gmail.com>
2023-07-26 12:24:22 +0100

    creat a program that concat two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - print a string,
+ * @dest: string 1
+ * @src: string 2
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+
+char *_strcat(char *dest, char *src);
