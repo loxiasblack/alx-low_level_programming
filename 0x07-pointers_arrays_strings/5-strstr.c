d97260fad9730df02a397076696b05253b3e4075
ilias <iliaschokairi@gmail.com>
2023-08-01 19:01:00 +0100

    creat my own strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,18 +8,26 @@
 char *_strstr(char *haystack, char *needle)
 {
 	char *current_needle;
+	char *current_haystack;
 
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
 	while (*haystack)
 	{
 		current_needle = needle;
+		current_haystack = haystack;
 
-		while (*current_needle)
+		while (*current_needle && *current_haystack &&
+		*current_haystack == *current_needle)
 		{
-			if (*haystack == *needle)
-			{
-				return (haystack);
-			}
+			current_haystack++;
 			current_needle++;
+		}
+		if (*current_needle == '\0')
+		{
+			return (haystack);
 		}
 		haystack++;
 	}
