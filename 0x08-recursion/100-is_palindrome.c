b5426bd9028c40e4692d53eee259e0df02135858
ilias <iliaschokairi@gmail.com>
2023-08-03 20:45:15 +0100

    creat a recursion program that determine if string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,17 +5,31 @@
  * @s: string
  * Return: 1 or 0
 */
+int function (char *s, int i, int l);
 int is_palindrome(char *s)
 {
 	int i = 0;
 	int l = strlen(s) - 1;
 
-	while (l > i)
-	{
-		if (s[i++] != s[l--])
-		{
-			return (0);
-		}
-	}
-	return (1);
+	if (*s)
+    {
+        return function(s, i, l);
+    }
+    return 0;
+}
+int function (char *s, int i, int l)
+{
+    if (NULL == s || l < 0 || i < 0)
+    {
+        return (0);
+    }
+    if (i >= l)
+    {
+        return (1);
+    }
+    if (s[i] == s[l])
+    {
+        return function(s, i+1, l-1);
+    }
+    return 0;    
 }
