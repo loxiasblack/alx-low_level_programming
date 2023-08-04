8bcae541157a14b7190afe58e762752fbfab13bb
ilias <iliaschokairi@gmail.com>
2023-08-04 18:02:01 +0100

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that multiple argument
+ * @argc: the number of argument
+ * @argv: the string pointer
+ * Return: 0 succes:
+*/
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int mult = 1;
+
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			mult = mult * atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
