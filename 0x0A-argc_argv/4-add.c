c7cf2a709b70fe50d4a23bdb4949e2d2a53420a1
ilias <iliaschokairi@gmail.com>
2023-08-04 18:36:05 +0100

    creat a program that add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that add numbers
+ * @argc: the number of argument
+ * @argv: the pointer of array
+ * Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int add = 0;
+
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				add += atoi(argv[i]);
+			}
+			i++;
+		}
+		printf("%d\n", add);
+		return (0);
+	}
+	else
+	{
+		printf("0\n");
+		return (0);
+	}
+}
