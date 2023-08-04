ca0fe31396d1f1c46969b411f4724341ab8abc1c
ilias <iliaschokairi@gmail.com>
2023-08-04 17:22:32 +0100

    creat a program that count the number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - function that return the number of vector
+ * @argc: the count
+ * @argv: the number of argument
+ * Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		while (argv[i] < argv[argc - 1])
+		{
+			sum += 1;
+			i++;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", i);
+	}
+	return (0)
+}
