8f1d09b06908cad3584b44fe0fae6a4dec9f67c7
ilias <iliaschokairi@gmail.com>
2023-08-04 17:28:35 +0100

    creat a librarie static

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -25,3 +26,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
