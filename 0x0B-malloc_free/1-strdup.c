2819b509178fe86e9841e81086c99a54d59df0c0
ilias <iliaschokairi@gmail.com>
2023-08-07 19:55:15 +0100

    creat a dup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - function that duplicate string
+ * @str: string to duplicate
+ * Return: s success
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int len;
+	int i;
+
+	len = strlen(str);
+
+	s = malloc(sizeof(char) * (len + 1));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
+}
