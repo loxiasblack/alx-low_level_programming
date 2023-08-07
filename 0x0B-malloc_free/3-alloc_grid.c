7631e2846e422e079cb207f616ac2eef63bf6f7c
ilias <iliaschokairi@gmail.com>
2023-08-07 22:33:51 +0100

    creat a program

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	int j;
 	int k;
 
-	grid = malloc(sizeof(*grid) * width);
+	grid = malloc(sizeof(*grid) * (width + 1));
 
 	if (grid == NULL || width <= 0 || height <= 0)
 	{
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = malloc(sizeof(**grid) * (height + 1));
+		grid[i] = malloc(sizeof(**grid) * height);
 
 		if (grid[i] == NULL)
 		{
