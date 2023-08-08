292b430165d3c5eb63ef4efcc62b4b2fac8d0a01
ilias <iliaschokairi@gmail.com>
2023-08-08 13:15:19 +0100

    creat a string of argument

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - function that creat a string of av
+ * @ac: argument count
+ * @av: argument vector
+ * Return: argc success
+*/
+char *argstostr(int ac, char **av)
+{
+	char *arg;
+	int i;
+	int total_length = 0;
+	int current_position = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		total_length += strlen(av[i]);
+	}
+	total_length += 1;
+	arg = malloc(total_length * sizeof(char));
+
+	if (arg == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(arg + current_position, av[i]);
+		current_position += strlen(av[i]);
+		arg[current_position] = '\n';
+		current_position++;
+	}
+	arg[current_position - 1] = '\n';
+	return (arg);
+}
