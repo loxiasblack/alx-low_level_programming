88cd1904a430cfd456b9e76773289cd219dee902
ilias <iliaschokairi@gmail.com>
2023-08-10 20:45:29 +0100

    creat a range array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_range - function that creat an array from min to
+ * max
+ * @min: the first number in the array
+ * @max: the last number int the array
+ * Return: the array
+*/
+int *array_range(int min, int max)
+{
+	int *array;
+	int len;
+	int i;
+
+	len = max - min;
+	array = malloc(sizeof(int) * (len + 1));
+	if (array == NULL || min > max)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (min <= max)
+	{
+		*(array + i) = min;
+		min++;
+		i++;
+	}
+	return (array);
+}
