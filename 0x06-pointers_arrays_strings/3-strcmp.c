130e73d6abe59b433e5273681531fd410ac21bd1
ilias <iliaschokairi@gmail.com>
2023-08-11 19:35:59 +0100

    creat my strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int result;
 
-	while (s1[i] == s2[i])
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] == '\0')
 	{
-		if (s1[i] == '\0')
-		{
-			return (result = 0);
-		}
 		i++;
 	}
-	if (s1[i] != s2[i])
+	if (s1[i] == '\0' && s2[i] == '\0')
 	{
-		result = s1[i] - s2[i];
+		return (0);
 	}
-	return (result);
+	else
+	{
+		return (s1[i] - s2[i]);
+	}
 }
