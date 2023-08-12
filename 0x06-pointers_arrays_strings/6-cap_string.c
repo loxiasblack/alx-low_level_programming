169d7f415d8a5a4b54d01afaf7922297854daa4d
ilias <iliaschokairi@gmail.com>
2023-08-12 10:28:20 +0100

    capitalize word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,26 +6,23 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i])
+	i = 0;
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i] == '0')
-			{
-				str -= 32;
-			}
-			if (i > 0 && (str[i - 1] == '\n' || str[i - 1] == ','
-			|| str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '\"' ||
-			str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' ||
-			str[i - 1] == '{' || str[i - 1] == '}' || str[i - 1] == '(' ||
-			str[i - 1] == ')' || str[i - 1] == ' '))
+			if (i == 0)
 			{
 				str[i] -= 32;
 			}
-			if (i > 0 && (str[i - 1] == '\t' || str[i - 1] == '\'' ||
-			str[i - 1] == '\\'))
+			if (i != 0 && (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}'))
 			{
 				str[i] -= 32;
 			}
