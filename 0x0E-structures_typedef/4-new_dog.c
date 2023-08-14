bcb3c2003f8cc3a6cebd25f35e939d2d1058d338
ilias <iliaschokairi@gmail.com>
2023-08-14 23:45:54 +0100

    creat a new struct of dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
 #include <string.h>
+char *_strdup(char *str);
 /**
  * new_dog - function that creat a new strcut dog
  * @name: name of the dog
@@ -18,13 +19,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	my_dog->name = strdup(name);
+	my_dog->name = _strdup(name);
 	if (my_dog->name == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
-	my_dog->owner = strdup(owner);
+	my_dog->owner = _strdup(owner);
 	if (my_dog->owner == NULL)
 	{
 		free(my_dog->name);
@@ -33,4 +34,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	my_dog->age = age;
 	return (my_dog);
+}
+/**
+ * _strdup - function that duplicate string
+ * @str: string to duplicate
+ * Return: s success
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int len;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = strlen(str);
+	s = malloc(sizeof(char) * (len + 1));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
 }
