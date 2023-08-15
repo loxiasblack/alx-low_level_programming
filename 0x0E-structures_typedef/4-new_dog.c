a040b893731bd1407be9b1185ec9fc5679dc521a
ilias <iliaschokairi@gmail.com>
2023-08-15 20:17:54 +0100

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,69 +1,32 @@
 #include <stdlib.h>
 #include "dog.h"
-#include <string.h>
-char *_strdup(char *str);
 /**
- * new_dog - function that creat a new strcut dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner
- * Return: new struc dog created
+ * new_dog - creat a new variable of struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ * Return: d succes;
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
+	dog_t *d;
 
-	my_dog = malloc(sizeof(dog_t));
-
-	if (my_dog == NULL)
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	if (d->name == NULL)
 	{
+		free(d);
 		return (NULL);
 	}
-	my_dog->name = _strdup(name);
-	if (my_dog->name == NULL)
+	d->age = age;
+	d->owner = owner;
+	if (d->owner == NULL)
 	{
-		free(my_dog);
+		free(d->name);
+		free(d);
 		return (NULL);
 	}
-	my_dog->owner = _strdup(owner);
-	if (my_dog->owner == NULL)
-	{
-		free(my_dog->name);
-		free(my_dog);
-		return (NULL);
-	}
-	my_dog->age = age;
-	return (my_dog);
-}
-/**
- * _strdup - function that duplicate string
- * @str: string to duplicate
- * Return: s success
-*/
-char *_strdup(char *str)
-{
-	char *s;
-	int len;
-	int i;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	len = strlen(str);
-	s = malloc(sizeof(char) * (len + 1));
-
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	while (i < len)
-	{
-		s[i] = str[i];
-		i++;
-	}
-	s[i] = '\0';
-
-	return (s);
+	return (d);
 }
