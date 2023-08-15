b372b6656f771d3832faca403a7804154cafbc9c
ilias <iliaschokairi@gmail.com>
2023-08-15 20:23:29 +0100

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h>
+char *_strdup(char *str);
 /**
  * new_dog - creat a new variable of struct dog
  * @name: name of dog
@@ -14,14 +16,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	d->name = _strdup(name);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
 	d->age = age;
-	d->owner = owner;
+	d->owner = _strdup(owner);
 	if (d->owner == NULL)
 	{
 		free(d->name);
@@ -29,4 +31,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	return (d);
+}
+/**
+ * _strdup - duplicate a string in another place in memory
+ * @str: str to duplicate
+ * Return: s success
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int len;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = strlen(str);
+	s = malloc(sizeof(char) * (len + 1));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+
+	return (s);
 }
