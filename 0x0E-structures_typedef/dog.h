d1458544f1860f5bbdfaaa4a6ead1e614f866f02
ilias <iliaschokairi@gmail.com>
2023-08-15 19:32:28 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - struct for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
 #endif
