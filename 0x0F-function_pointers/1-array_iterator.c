915a21434fd309dd5a86cbc978bcb35a0df74ecc
ilias <iliaschokairi@gmail.com>
2023-08-16 12:52:55 +0100

    array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long i;
 
-	if (action == NULL)
+	if (action == NULL || array == NULL)
 		return;
 	for (i = 0; i < size; i++)
 	{
