2ccd929899f742fed03dce77ceb474866af45328
ilias <iliaschokairi@gmail.com>
2023-08-16 19:58:38 +0100

    creat a program that return a result of operation between two numbers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	op = argv[2];
-	if (!(get_op_func(op)))
+	if (op[1] != '\0' || !(get_op_func(op)))
 	{
 		printf("Error\n");
 		exit(99);
