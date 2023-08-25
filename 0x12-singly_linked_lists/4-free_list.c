4b143a4d05fe3c6e50f32ed1797d64d1601ff2cb
ilias <iliaschokairi@gmail.com>
2023-08-25 17:01:04 +0100

    lets free nodes

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * free_list - function that free linked list
+ * @head: firat node in the linked list
+ */
 void free_list(list_t *head)
 {
-    list_t *tmp;
-    while (head != NULL)
-    {
-        tmp = head->next;
-        free(head->str);
-        free(head);
-        head = tmp;
-    }
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
 }
