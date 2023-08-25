69a1fd4ec266087cc5ca946427dcf4dd26a058aa
ilias <iliaschokairi@gmail.com>
2023-08-25 11:39:06 +0100

    test list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 /**
- * list_len - function that count the
- * number of node in the singly linked
- * list
- * @h: pointer to the header
- * Return: the count
+ * list_len - function that return
+ * the number of node inside the
+ * linked list
+ * @h: the node of linked list
+ * Return: the len (number of nodes);
 */
 size_t list_len(const list_t *h)
 {
-	size_t i = 0;
+	size_t len = 0;
 
 	while (h)
 	{
 		i++;
 		h = h->next;
 	}
-	return (i);
+	return (len);
 }
