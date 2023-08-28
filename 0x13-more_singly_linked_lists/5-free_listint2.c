58fcd94c75a328b6843a1f155edd7f2a4e03a105
ilias <iliaschokairi@gmail.com>
2023-08-28 10:56:21 +0100

    free_list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function that free list
+ * @head: adresse of head
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+}
