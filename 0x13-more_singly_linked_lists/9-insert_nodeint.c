65fc3797a2c12fd74f3fe287e9952bf2213cce3a
ilias <iliaschokairi@gmail.com>
2023-08-28 20:47:55 +0100

    try again on this function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,6 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			}
 			count++;
 		}
+		if (tmp->next == NULL && (count < (idx - 2)))
+			return (NULL);
 	}
 	return (node);
 }
