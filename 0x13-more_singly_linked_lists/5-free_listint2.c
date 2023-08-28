cae15317aaced676bbdac2b7fef260542c7f61bd
ilias <iliaschokairi@gmail.com>
2023-08-28 20:23:23 +0100

    try again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (*head == NULL)
+		return;
 	while (*head)
 	{
 		tmp = *head;
 		*head = tmp->next;
 		free(tmp);
 	}
-	if (*head == NULL)
-		return;
 }
