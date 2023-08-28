82a762a3ea3e1072d0aae430010febfa2d3dca33
ilias <iliaschokairi@gmail.com>
2023-08-28 20:43:15 +0100

    fix the function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp;
 
 	node = malloc(sizeof(listint_t));
-	if (node == NULL || idx < 1)
+	if (node == NULL)
 		return (NULL);
 	node->n = n;
 	node->next = NULL;
