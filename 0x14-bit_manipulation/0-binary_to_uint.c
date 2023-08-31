160a2302aca24349037c2b7c0005c7fb82f729fb
ilias <iliaschokairi@gmail.com>
2023-08-31 19:20:13 +0100

    convert binary to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _pow_recursion - function that return
+ * the power of two number
+ * @x: the base number
+ * @y: the exp number
+ * Return: the power by recursion
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
+/**
+ * binary_to_uint - convert from binary to
+ * decimal
+ * @b: pointer to a string of number
+ * Return: decimal success
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	int  place = 0;
+	unsigned int decimal = 0, rem = 0;
+	int num;
+
+	if (b == NULL)
+		return (0);
+	num = atoi(b);
+	while (num)
+	{
+		rem = num % 10;
+		if (rem)
+		{
+			decimal = decimal + (_pow_recursion(2, place));
+		}
+		num = num / 10;
+		place++;
+	}
+	return (decimal);
+}
