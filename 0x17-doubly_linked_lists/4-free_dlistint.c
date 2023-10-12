d48f1eb7c6aeb47c6844c1da863521c860acdd5d
ilias <iliaschokairi@gmail.com>
2023-10-12 20:44:19 +0100

    freee dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked list.
+ * @head: pointer to header
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
