4001b39779532ffcfe1f6545e097bc0bbc481bd0
ilias <iliaschokairi@gmail.com>
2024-02-06 01:16:33 +0100

    try again

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,7 +20,7 @@ int interpolation_search(int *array, size_t size, int value)
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 		* (value - array[low]));
-		if (pos >= size)
+		if (pos > size)
 		{
 			printf("Value checked array[%ld] is out of range\n", pos);
 			return (-1);
