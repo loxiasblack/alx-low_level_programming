bbf96c846c65edeb9a2fc2741111b146266bfff1
ilias <iliaschokairi@gmail.com>
2024-04-09 16:12:37 +0000

    fux

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,12 +12,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t step = sqrt(size);
 	size_t temp = 0;
 
-	if (array[min(step, size) - 1] > value)
-	{
-		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
-		temp = prev;
-		prev += step;
-	}
+	
 	while (array[min(step, size) - 1] < value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
