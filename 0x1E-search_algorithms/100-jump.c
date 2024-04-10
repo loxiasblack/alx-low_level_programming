771cabe26703bb6647f8f5389420d85d3137ffd9
ilias <iliaschokairi@gmail.com>
2024-04-10 16:11:05 +0000

    new algorithme

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,22 +11,20 @@ int jump_search(int *array, size_t size, int value)
 	int prev = 0;
 	size_t step = sqrt(size);
 
-	if (array[min(step, size) - 1] > value)
+    printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+	while (array[min(step, size)] < value)
 	{
-	    printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-	}
-
-	while (array[min(step, size) - 1] < value)
-	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		prev = step;
 		step += sqrt(size);
+        printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		if (step > size)
 		{
+            printf("Value found between indexes [%d] and [%ld]\n", prev, step);
 			printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-			break;
+            return (-1);
 		}
 	}
+    
 	printf("Value found between indexes [%d] and [%ld]\n", prev, step);
 
 	while (array[prev] < value)
@@ -34,7 +32,7 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		prev++;
 		if (prev == min(step, size))
-			return (-1);
+			break;
 	}
 	printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 	if (array[prev] == value)
