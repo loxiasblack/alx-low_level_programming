0959155090dd08050b8e06237baa32750bffd29a
ilias <iliaschokairi@gmail.com>
2024-04-12 17:25:04 +0000

    back to the first algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,23 +10,21 @@ int jump_search(int *array, size_t size, int value)
 {
 	int prev = 0;
 	size_t step = sqrt(size);
-	int tmp = 0;
 
 	printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 	while (array[min(step, size)] < value)
 	{
-		tmp = prev;
 		prev = step;
 		step += sqrt(size);
+        printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		if (step > size)
 		{
-			prev = tmp;
-			step = size;
-			break;
+            printf("Value found between indexes [%d] and [%ld]\n", prev, step);
+			printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+			return (-1);
 		}
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 	}
-
+    
 	printf("Value found between indexes [%d] and [%ld]\n", prev, step);
 
 	while (array[prev] < value)
@@ -34,10 +32,9 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 		prev++;
 		if (prev == min(step, size))
-		{
 			break;
-		}
 	}
+	printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 	if (array[prev] == value)
 		return (prev);
 	else
